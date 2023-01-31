b3718f71443047b378ad07fb5f09ec0ccd3cdb68
Peyang <peyang@peya.tokyo>
2023-01-31 20:33:35 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# KPMUpgrader
+
+## 概要
+
+[TeamKUNPluginManager](https://github.com/TeamKUN/TeamKUNPluginManager)\(KPM) を自動でアップグレードするプラグインです。
+
+詳しくは[こちら](https://kpm.kunlab.org/docs/use-kpm/commands/#upgrade-kpm)のドキュメントを参照してください。
+
+## このプラグインを直接インストールしないでください
+
+このプラグインは、 KPM が自動でインストールします。ユーザがこれを直接インストールした場合、最悪サーバが動作しなくなる可能性があります。  
+インストールしてしまった場合は、速やかに jar ファイルを削除しサーバを再起動してください。
+
---
+++ src/main/java/net/kunmc/lab/kpm/kpmupgrader/KPMUpgraderPlugin.java
@@ -0,0 +1,58 @@
+package net.kunmc.lab.kpm.kpmupgrader;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+public final class KPMUpgraderPlugin extends JavaPlugin implements CommandExecutor
+{
+    private final UpgradeImpl impl;
+
+    public KPMUpgraderPlugin()
+    {
+        this.impl = new UpgradeImpl(this);
+    }
+
+    @Override
+    public void onEnable()
+    {
+        if (Bukkit.getPluginManager().getPlugin("TeamKunPluginManager") == null)
+        {
+            this.getLogger().severe("TeamKunPluginManager がインストールされていません。このプラグインを削除してください。");
+            this.getLogger().severe("The server doesn't have TeamKunPluginManager. Please remove this plugin.");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        this.impl.initDaemon();
+        Objects.requireNonNull(this.getCommand("kpm-upgrade-internal")).setExecutor(this);
+    }
+
+    private boolean commandError(CommandSender sender)
+    {
+        sender.sendMessage("You cannot use this command.");
+        return true;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @Nonnull @NotNull String[] args)
+    {
+        if (!(sender instanceof ConsoleCommandSender))
+            return this.commandError(sender);
+
+        if (args.length == 0)
+            return this.commandError(sender);
+
+        String toVersion = args[0];
+
+        this.impl.runUpgrade(toVersion);
+        return true;
+    }
+}
